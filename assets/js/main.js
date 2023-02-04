1249c80e6d9969c429a5ae85b1eb47d8d556cf5b
mehebbetbabayeva <mhbbtbabaeva97@gmail.com>
2023-02-04 15:51:34 +0400

    finish entrance page

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,54 @@ document.querySelector(".backmenu").addEventListener("click" , ()=>{
 })
 //menuya click olduqda
 
+
+
+document.querySelector(".signup").addEventListener("click" ,()=>{
+   let sign =document.querySelector(".sign");
+   sign.style.left="0";
+})
+
+document.querySelector(".signupmain").addEventListener("click" , ()=>{
+let firstname = document.querySelector(".firstname").value;
+let lastname = document.querySelector(".lastname").value;
+let email = document.querySelector(".email").value;
+let number = document.querySelector(".number").value;
+let age = document.querySelector(".age").value;
+let password = document.querySelector(".password").value;
+let passwordconfirm = document.querySelector(".passwordconfirm").value;
+let signupmain = document.querySelector(".signupmain");
+
+
+if(firstname.length=="" && lastname.length=="" && email.length=="" && number.length=="" &&
+age.length=="" && password.length==""){
+  alert("fill in the boxes");
+}else{ if(password==passwordconfirm){ 
+    let data ={
+        firstname,
+        lastname,
+        email,
+        number,
+        age ,
+        password,
+     }
+     console.log(data)
+      firstname = document.querySelector(".firstname").value="";
+      lastname = document.querySelector(".lastname").value="";
+      email = document.querySelector(".email").value="";
+      number = document.querySelector(".number").value="";
+      age = document.querySelector(".age").value="";
+      password = document.querySelector(".password").value="";
+      passwordconfirm = document.querySelector(".passwordconfirm").value="";
+      let sign =document.querySelector(".sign");
+      sign.style.left="-100%";
+   }
+   else{
+     alert("the passwords are not the same")
+   }}
+  
+    })
+   document.querySelector(".sign").addEventListener("click",()=>{
+    let sign =document.querySelector(".sign");
+    sign.style.left="-100%";
+   })
+//signupa click olduqda
